25808c1fd3c89285f14dc6f831b46caea26623eb
abhargav-mw <abhargav@mathworks.com>
2021-12-09 13:28:01 +0530

    update runAllTest

--- runAllTests.m
+++ runAllTests.m
@@ -18,6 +18,6 @@ runner.addPlugin(CodeCoveragePlugin.forFolder({'src/folderA', 'src/folderB'}, 'I
 results = runner.run(suite);
 
 % Generate Zip files
-zip('myfiles.zip','myfolder');
+zip('covReport.zip','covReport');
 nfailed = nnz([results.Failed]);
 assert(nfailed == 0, [num2str(nfailed) ' test(s) failed.']);
